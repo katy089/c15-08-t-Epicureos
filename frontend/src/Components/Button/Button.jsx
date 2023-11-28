36beaf922d4cffbcc438f361522727b40454a2ba
fanofrisoni <fanofrisoni4@gmail.com>
2023-11-28 16:11:36 -0600

    Button change

--- frontend/src/Components/Button/Button.jsx
+++ frontend/src/Components/Button/Button.jsx
@@ -3,7 +3,7 @@ import "./button.css";
 
 function Button(props) {
   const { bColor, tColor, text, iColor, click } = props;
-  const [isHover, setIsHover] = useState();
+  const [isHover, setIsHover] = useState(false);
   const buttonStyle = {
     backgroundColor: isHover ? iColor : bColor,
     color: tColor,
