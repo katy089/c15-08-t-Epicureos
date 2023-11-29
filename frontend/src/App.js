15ed75f37c57ad68c92a5bcdea1efc76bbaa3e8c
Katya Anco <ladygetya@gmail.com>
2023-11-29 14:56:56 -0500

    add change App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,39 @@
-import "./App.css";
-// import Login from "./Pages/Login";
-import Signup from "./Pages/Signup";
+import React from 'react';
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+import Signup from './Pages/Signup.jsx'
+import Login  from './Pages/Login.jsx'
+
 
 function App() {
   return (
-    <div className="App">
-      <Signup></Signup>
-    </div>
+    <Router>
+      <div className="App">
+          <Route exact path="/" component={Signup} />
+          <Route exact path="/login" component={Login} />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+// import "./App.css";
+// // import Login from "./Pages/Login";
+// import Signup from "./Pages/Signup";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Signup></Signup>
+//     </div>
+//   );
+// }
+
+// export default App;
