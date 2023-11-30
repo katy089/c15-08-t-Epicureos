09fc749691063f1964e860c5565c6a1ab3a65513
AngelBlackBlue <angel.blackbkue@hotmail.com>
2023-11-30 20:55:09 -0300

    mailer creado

---
+++ backend/src/configs/mailer.js
@@ -0,0 +1,20 @@
+const nodemailer = require('nodemailer')
+
+const smptServices = process.env.SMPT_SERVICES
+const smptHost = process.env.SMPT_HOST
+const smptPort = process.env.SMPT_PORT
+const smptAccount = process.env.SMPT_ACCOUNT
+const smptPassword = process.env.SMPT_PASSWORD
+
+const transporter = nodemailer.createTransport({
+    service: smptServices,
+    host: smptHost,
+    port: smptPort,
+    secure: true,
+    auth: {
+        user: smptAccount,
+        pass: smptPassword
+    }
+})
+
+module.exports = { transporter }
--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -2,8 +2,12 @@ const { findEmail, createUser } = require('../services/user.service')
 
 const registerService = async (body) => {
     const { email } = body
+    
     const verifyEmail = await findEmail(email)
     if (verifyEmail) { throw new Error('USER_EXIST') }
+    
+    const validator =generateRandomNumber().toString()
+
     const newUser = await createUser({ ...body })
     return newUser
 }
