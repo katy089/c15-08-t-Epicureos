d516feb0b0a90d6fe8ac6f304d5f1dad680b2e60
rb555 <intzabai@gmail.com>
2023-12-05 18:54:58 -0300

    cambios token

--- backend/src/libs/handleToken.js
+++ backend/src/libs/handleToken.js
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (uid) => {
-    const expiresIn = 1000 * 60 * 15;
+    const expiresIn = '7d';
     const token = jwt.sign({ uid }, process.env.JWT_SECRET, { expiresIn });
     return { token, expiresIn };
 };
 
 export const generateRefreshToken = (uid, res) => {
-    const expiresIn = 1000 * 60 * 60 * 24 * 30;
+    const expiresIn = '7d';
     const refreshToken = jwt.sign({ uid }, process.env.JWT_REFRESH, {
         expiresIn,
     });
