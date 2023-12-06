eac7646ec18266f39fdb0ae477673dcf66037b34
NicolasRaffa <djnikorc@gmail.com>
2023-12-06 18:26:03 -0300

    Disponibilidad de reservas

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,10 @@
 const { registerTokenController, validateController, loginController, recoverPasswordController } = require('../controllers/auth.controller')
-
+const { validateRegister } = require('../validator/auth.validator')
 const { Router } = require('express')
 
 const router = Router()
 
-router.post('/register', registerTokenController)
+router.post('/register', validateRegister, registerTokenController)
 router.post('/validate', validateController)
 router.post('/login', loginController)
 router.post('/recover/:email', recoverPasswordController)
