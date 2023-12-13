d5224fb8f84a845d03d3699d0d1c02f1d2b384c8
NicolasRaffa <djnikorc@gmail.com>
2023-12-13 17:50:52 -0300

    Reservation

--- backend/src/routes/booking.routes.js
+++ backend/src/routes/booking.routes.js
@@ -5,7 +5,7 @@ const { Router } = require('express')
 const router = Router()
 
 router.post('/createBooking', reservationController )
-router.post('/showReservation', showReservationController )
+router.get('/showReservation/:userId', showReservationController )
 router.delete('/deleteReservation', deleteReservationController )
 
 module.exports = router
