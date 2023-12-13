7b29cf2cc85e33896ebb43435b301df7dfe286cb
NicolasRaffa <djnikorc@gmail.com>
2023-12-12 21:06:46 -0300

    Reservation

--- backend/src/services/availability.service.js
+++ backend/src/services/availability.service.js
@@ -1,3 +1,4 @@
+const { transformDate } = require("../helpers/transformDate.helper")
 const Availability = require("../models/availability.model")
 const { Op } = require('sequelize')
 
@@ -11,34 +12,42 @@ const stripAvailability = async (where) => {
     }
     const subtractionStrip1 = session.strip1 - session.people1
     const subtractionStrip2 = session.strip2 - session.people2
+    const amountOfPlaces = {
+        strip1: subtractionStrip1,
+        strip2: subtractionStrip2
+    }
     if (subtractionStrip1 === 0 && subtractionStrip2 === 0) {
-        return false
+        return amountOfPlaces
     } else {
-        const amountOfPlaces = {
-            strip1: subtractionStrip1,
-            strip2: subtractionStrip2
-        }
         return amountOfPlaces
     }
-
 }
 
 const dateAvailability = async () => {
     const result = await Availability.findAll({
         where: {
+            status: "enabled",
             [Op.or]: [
-              { people1: { [Op.gte]: 0, [Op.lt]: 40 } },
-              { people2: { [Op.gte]: 0, [Op.lt]: 40 } },
+                { people1: { [Op.gte]: 0, [Op.lt]: 40 } },
+                { people2: { [Op.gte]: 0, [Op.lt]: 40 } },
             ],
-          },
+        },
     })
-    const formattedResult = result.map (availability => availability.date)
-
-
+    const formattedResult = result.map(availability => availability.date)
     return formattedResult
 }
 
+const findDate = async (where) => await Availability.findOne({ where })
 
-const addAvailability = async (data) => await Availability.create(data)
 
-module.exports = { dateAvailability, addAvailability, stripAvailability }
+const addAvailability = async (data) => {
+    const { date, ...restData } = data
+    const dateTransformed = transformDate(date)
+    const newAvailability = { date: dateTransformed, ...restData }
+    const availability = await Availability.create(newAvailability)
+    return availability
+}
+
+
+
+module.exports = { dateAvailability, addAvailability, stripAvailability, findDate }
