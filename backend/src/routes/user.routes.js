661b88d8177b940b799b8f758683d2c2b26866ee
rb555 <intzabai@gmail.com>
2023-12-15 14:48:42 -0300

    user update

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,9 +1,37 @@
 const { profileController } = require('../controllers/user.controller')
+const express = require('express');
+const router = express.Router();
+const {updateUserService} = require('../services/user.service.js')
 
-const { Router } = require('express')
+//Get user profile
+router.get('/profile', profileController);
 
-const router = Router()
+//Update user data
+router.put('/users/:email', async (req, res) => {
+    try {
+        const { email } = req.params;
+        const updatedUserData = req.body;
 
-router.get('/profile', profileController)
+        const updatedUser = await updateUserService(email, updatedUserData);
 
-module.exports = router
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+//New route for updating user data
+router.put('/users/:userId', async (req,res) => {
+    try {
+        const { userId } = req.params;
+        const updatedUserData = req.body;
+
+        const updatedUser = await updateUserService(userId, updatedUserData);
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+});
+
+module.exports = router;
--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -12,11 +12,37 @@ const showProfile = async (user) => {
         createdAt: result.createdAt
     }
     return session
-}
+};
+
 const createUser = async (data) => {
-    const newUser = await User.create(data)
-    return newUser
-}
+    const newUser = await User.create(data);
+    return newUser;
+};
+
+const updateUserData = async (userId, updatedData) => {
+    try {
+        const user = await User.findByPk(userId);
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        // Update user data
+        await user.update(updatedData);
+
+        const updatedUser = {
+            id: user.id,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            email: user.email,
+            createdAt: user.createdAt,
+        };
+
+        return updatedUser;
+    } catch (error) {
+        throw new Error(`Error updating user data: ${error.message}`);
+    }
+};
 
 
-module.exports = { findData, createUser, showProfile }
+module.exports = { findData, createUser, showProfile, updateUserData }
